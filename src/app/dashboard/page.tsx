851696a0e1217ecc45454fde8366dc81d558df11
Yutaro Negi <souza_yutaro@hotmail.com>
2024-12-01 16:27:16 -0300

    feat: add basic dashboard with logout button

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { LogoutButton } from "./components/LogoutButton";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <LogoutButton />
+    </div>
+  );
+};
+
+export default Dashboard;
